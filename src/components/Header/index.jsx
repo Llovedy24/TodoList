64ebdad7737d8b56015ab8553399d6e1e854fa2f
Lovedy Louis-Jean <lovedylouisjean@LovedysacStudio.lan1>
2023-01-07 11:31:17 -0500

    first

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,32 @@
+import todoLogo from '../../assets/todoLogo.svg';
+import styles from './header.module.css';
+import { AiFillPlusCircle } from 'react-icons/ai';
+import{ useState } from 'react';
+
+
+export function Header({ handleAddTask }) {
+    const [title, setTitle] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        handleAddTask(title);
+        setTitle('');
+    }
+
+    function onChangeTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    return (
+        <header className={styles.header}>
+            <img src={todoLogo} />
+
+            <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+                <input placeholder="Add a new task" type="text" onChange={onChangeTitle} value={title}/>
+                
+                <button>Create <AiFillPlusCircle size={20} /></button>
+            
+            </form>
+        </header>
+    )
+}
